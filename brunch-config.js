2b29ef80c03ac7abcddb0a682ea41d0529fb11ec
John Bohn <jjbohn@gmail.com>
2016-03-07 09:00:16 -0500

    Use bootstrap and do some basic setup

--- brunch-config.js
+++ brunch-config.js
@@ -49,6 +49,12 @@ exports.config = {
 
   // Configure your plugins
   plugins: {
+    sass: {
+      mode: "native",
+      options: {
+        includePaths: ["node_modules/bootstrap/scss"]
+      }
+    },
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/web\/static\/vendor/]
@@ -62,9 +68,6 @@ exports.config = {
   },
 
   npm: {
-    enabled: true,
-    // Whitelist the npm deps to be pulled in as front-end assets.
-    // All other deps in package.json will be excluded from the bundle.
-    whitelist: ["phoenix", "phoenix_html"]
+    enabled: true
   }
 };
