0346e206476583cd5642cbc86bd6e83cf5d6d9cb
John Bohn <jjbohn@gmail.com>
2016-03-08 18:17:22 -0500

    RSS proxy

--- web/controllers/quick_link_controller.ex
+++ web/controllers/quick_link_controller.ex
@@ -1,15 +1,18 @@
 defmodule Podcaster.QuickLinkController do
   use Podcaster.Web, :controller
 
-	def itunes(conn, _params) do
+  def itunes(conn, _params) do
     redirect conn, external: "https://itunes.apple.com/us/podcast/state-development-podcast/id1089601796?mt=2"
   end
 
-	def rss(conn, _params) do
-    redirect conn, external: "https://simplecast.com/podcasts/1739/rss"
+  def rss(conn, _params) do
+    {:ok, response} = HTTPoison.get("https://simplecast.com/podcasts/1739/rss")
+    render conn
+    |> put_resp_content_type("text/xml")
+    |> text response.body
   end
 
-	def stitcher(conn, _params) do
+  def stitcher(conn, _params) do
     redirect conn, external: "http://www.stitcher.com/podcast/state-of-development-with-john-bohn/state-of-development-podcast-with-john-bohn"
   end
 
