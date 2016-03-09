e8c51634787e64832188aa0fa7ca1064763382dd
John Bohn <jjbohn@gmail.com>
2016-03-08 19:17:33 -0500

    Add tracking for subscriptions and follows

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,5 +1,6 @@
 import "phoenix_html";
 import "bootstrap";
+import $ from "jquery";
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -8,3 +9,23 @@ import "bootstrap";
 
 ga('create', 'UA-74228439-1', 'auto');
 ga('send', 'pageview');
+
+$(".trk__subscribe-with-itunes").click(() => {
+  ga("send", "event", ["Podcast"], ["Subscribe"], ["iTunes"])
+});
+
+$(".trk__subscribe-with-rss").click(() => {
+  ga("send", "event", ["Podcast"], ["Subscribe"], ["RSS"])
+});
+
+$(".trk__subscribe-with-stitcher").click(() => {
+  ga("send", "event", ["Podcast"], ["Subscribe"], ["Stitcher"])
+});
+
+$(".trk__follow-twitter").click(() => {
+  ga("send", "event", ["Social"], ["Follow"], ["Twitter"])
+});
+
+$(".trk__follow-facebook").click(() => {
+  ga("send", "event", ["Social"], ["Follow"], ["Facbook"])
+});
