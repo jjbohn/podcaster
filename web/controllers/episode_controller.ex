cc0d16db8e4785f37c952908db09d010c8a561a5
John Bohn <jjbohn@gmail.com>
2016-03-15 22:25:32 -0400

    Fix title and description meta

--- web/controllers/episode_controller.ex
+++ web/controllers/episode_controller.ex
@@ -6,7 +6,9 @@ defmodule Podcaster.EpisodeController do
     featured_episode = Enum.at(episodes, 0)
 
     render conn, "index.html", %{episodes: episodes,
-                                 featured_episode: featured_episode}
+                                 featured_episode: featured_episode,
+                                 title: "State of Development Podcast with John Bohn | Software Development | Programming | News",
+                                 description: "Need to know what's going on in the world of software development? Join John as he takes you through the week's programming/software development news."}
   end
 
   def show(conn, %{"episode_id" => episode_id}) do
@@ -16,6 +18,8 @@ defmodule Podcaster.EpisodeController do
     episode = Enum.find(episodes, &(&1.number == String.to_integer(episode_id)))
 
     render conn, "show.html", %{episode: episode,
-                                featured_episode: featured_episode}
+                                featured_episode: featured_episode,
+                                title: episode.title,
+                                description: episode.description}
   end
 end
